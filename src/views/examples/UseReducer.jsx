afa6196a8f47ffa1e217cbc7f11fa45b2d6cdbcd
Pedro Batista <pedrohenrique.sb07@gmail.com>
2022-04-04 21:59:36 -0300

    Desafio useReduce

--- src/views/examples/UseReducer.jsx
+++ src/views/examples/UseReducer.jsx
@@ -6,13 +6,26 @@ const initialState = {
     products: [],
     user: null,
     // foco...
-    number: 0
+    number: 0,
+    numberToAdd: 0, 
 }
 
+function numberToAdd(n){
+    var result = parseInt(n)
+    return Number.isNaN(result) ? 0 : result
+ }
+
+//adicionar n
 function reducer(state, action) {
+    console.log(state);
     switch(action.type) {
         case 'numberAdd2': return {...state, number: state.number + 2}
         case 'login': return {...state, user: { name: action.payload }}
+        case 'multiplyBy7': return {...state, number: state.number * 7}
+        case 'divideBy25': return {...state, number: state.number / 25}
+        case 'roundNumber': return {...state, number: parseInt(state.number) }
+        case 'setNumberToAdd': return {...state, numberToAdd: numberToAdd(action.payload)}
+        case 'sum': return {...state, number: state.number + state.numberToAdd}        
         default: return state
     }
 }
@@ -34,10 +47,15 @@ const UseReducer = (props) => {
                  <span className="text">Sem usuário</span>
                 }
                 
-                <span className="text">{state.number}</span>
+                <span className="text">{state.number}</span>                
                 <div>
                     <button className="btn" onClick={() => dispatch({type: 'numberAdd2'})}>+2</button>
                     <button className="btn" onClick={() => dispatch({type: 'login', payload: 'Pedro'})}>Login</button>
+                    <button className="btn" onClick={() => dispatch({type: 'multiplyBy7'})}>*7</button>
+                    <button className="btn" onClick={() => dispatch({type: 'divideBy25'})}>/25</button>
+                    <button className="btn" onClick={() => dispatch({type: 'roundNumber'})}>Arredondar</button>
+                    <input type="text" className="input" value={state.numberToAdd} onChange={e => dispatch({type: 'setNumberToAdd', payload: e.target.value})}/>
+                    <button className="btn" onClick={() => dispatch({type: 'sum'})}>Sum</button>
                 </div>                
             </div>
         </div>      
