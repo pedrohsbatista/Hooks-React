e365e1e7aa877410922bdb17c6d2f26af99c481b
Pedro Batista <pedrohenrique.sb07@gmail.com>
2022-04-05 21:37:06 -0300

    Hook personalizado

--- src/views/examples/UseCustom.jsx
+++ src/views/examples/UseCustom.jsx
@@ -2,10 +2,22 @@ import React from 'react'
 import PageTitle from '../../components/layout/PageTitle'
 import SectionTitle from '../../components/layout/SectionTitle'
 import { useCounter } from '../../hooks/UseCounter'
+import { useFetch } from '../../hooks/UseFetch'
 
 const UseRef = (props) => {
 
     const [count, inc, dec] = useCounter(100)
+
+    const url = "http://files.cod3r.com.br/curso-react/estados.json"
+    const response = useFetch(url)
+
+    function showStates(states) {
+        return states.map(state => <li key={state.nome}>{state.nome} - {state.sigla}</li> )
+    }
+
+    //fetch("http://files.cod3r.com.br/curso-react/estados.json")
+    //    .then(resp => resp.json())
+    //    .then(json => console.log(json))
 
     return (
         <div className="UseCustom">
@@ -21,6 +33,13 @@ const UseRef = (props) => {
                     <button className="btn" onClick={() => inc()}>+1</button>
                     <button className="btn" onClick={() => dec()}>-1</button>
                 </div>
+            </div>
+
+            <SectionTitle title="Exercício #02"></SectionTitle>
+            <div className="center">
+                <ul>
+                    {!response.loading ? showStates(response.data) : false}
+                </ul>
             </div>
         </div>
     )
